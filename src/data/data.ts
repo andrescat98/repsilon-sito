968d70de13d92b91db10154f5e2f0887ac0bf0ef
andrescat98 <andrescat98@gmail.com>
2026-03-04 12:28:20 +0100

    pagina album

---
+++ src/data/data.ts
@@ -0,0 +1,32 @@
+export const releases = [
+    {
+        title: "Onde",
+        type: "Album",
+        date: "2026",
+        cover: "/images/OndeLabel.png",
+        spotify: "https://open.spotify.com/intl-it/album/3ocNyROi9LVgQ8AwsB2KGw",
+    },
+    {
+        title: "Never Shine In Black",
+        type: "Album",
+        date: "2021",
+        cover: "/images/NsibLabel.jpg",
+        spotify: "https://open.spotify.com/intl-it/album/50K9CKxtFjCn60OC32Jhbx",
+    },
+    {
+        title: "Useless",
+        type: "Album",
+        date: "2020",
+        cover: "/images/UselessLabel.jpg",
+        spotify: "https://open.spotify.com/intl-it/album/0Tebv5WFPZP9Popddzyu1z",
+    },
+];
+
+export const events = [
+    {
+        date: "13 MAR 2026",
+        venue: "OFF TOPIC",
+        city: "Torino",
+        link: "https://dice.fm/partner/tickets/event/xe6yvl-situazione-13th-mar-off-topic-torino-tickets?dice_id=8553167&dice_channel=web&dice_tags=organic&dice_campaign=OFF+TOPIC+APS&dice_feature=mio_marketing&utm_source=web&utm_campaign=OFF+TOPIC+APS&utm_medium=mio_marketing"
+    },
+];
