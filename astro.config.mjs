565564dc97c754adfa0860e4cd67cee0c8599033
andrescat98 <andrescat98@gmail.com>
2026-03-04 12:54:19 +0100

    press kit

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import netlify from '@astrojs/netlify';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
