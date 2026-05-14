8f9e6e6c88229a523c2990ecbf3921b77fbe5784
andrescat98 <andrescat98@gmail.com>
2026-05-15 00:33:54 +0200

    messo location e nuovo evento

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,36 +1,45 @@
 export const releases = [
     {
-        title: "Onde",
-        type: "Album",
-        date: "2026",
-        cover: "/images/OndeLabel.png",
-        spotify: "https://open.spotify.com/intl-it/album/4RFPT1ej1rdmNrBhoxtayf",
-        page: "/onde",
+        title: 'Onde',
+        type: 'Album',
+        date: '2026',
+        cover: '/images/OndeLabel.png',
+        spotify: 'https://open.spotify.com/intl-it/album/4RFPT1ej1rdmNrBhoxtayf',
+        page: '/onde'
     },
     {
-        title: "Never Shine In Black",
-        type: "Album",
-        date: "2021",
-        cover: "/images/NsibLabel.jpg",
-        spotify: "https://open.spotify.com/intl-it/album/50K9CKxtFjCn60OC32Jhbx",
-        page: "/never-shine-in-black"
+        title: 'Never Shine In Black',
+        type: 'Album',
+        date: '2021',
+        cover: '/images/NsibLabel.jpg',
+        spotify: 'https://open.spotify.com/intl-it/album/50K9CKxtFjCn60OC32Jhbx',
+        page: '/never-shine-in-black'
     },
     {
-        title: "Useless",
-        type: "Album",
-        date: "2020",
-        cover: "/images/UselessLabel.jpg",
-        spotify: "https://open.spotify.com/intl-it/album/0Tebv5WFPZP9Popddzyu1z",
-        page: "/useless",
-    },
+        title: 'Useless',
+        type: 'Album',
+        date: '2020',
+        cover: '/images/UselessLabel.jpg',
+        spotify: 'https://open.spotify.com/intl-it/album/0Tebv5WFPZP9Popddzyu1z',
+        page: '/useless'
+    }
 ];
 
 export const events = [
     {
-        date: "13 MAR 2026",
-        dateISO: "2026-03-13",
-        venue: "OFF TOPIC",
-        city: "Torino",
-        link: "https://dice.fm/partner/tickets/event/xe6yvl-situazione-13th-mar-off-topic-torino-tickets?dice_id=8553167&dice_channel=web&dice_tags=organic&dice_campaign=OFF+TOPIC+APS&dice_feature=mio_marketing&utm_source=web&utm_campaign=OFF+TOPIC+APS&utm_medium=mio_marketing"
+        date: '13 MAR 2026',
+        dateISO: '2026-03-13',
+        name: 'OFF TOPIC',
+        location: 'Off Topic',
+        city: 'Torino',
+        link: 'https://dice.fm/partner/tickets/event/xe6yvl-situazione-13th-mar-off-topic-torino-tickets?dice_id=8553167&dice_channel=web&dice_tags=organic&dice_campaign=OFF+TOPIC+APS&dice_feature=mio_marketing&utm_source=web&utm_campaign=OFF+TOPIC+APS&utm_medium=mio_marketing'
     },
+    {
+        date: '22 MAG 2026',
+        dateISO: '2026-05-22',
+        name: 'DaBuDa MUSIC FEST',
+        location: 'Centro Giovanile Arka – Piazza Caselli 19',
+        city: 'Chieri',
+        link: 'https://www.instagram.com/p/DYRZ-sRiAwH/?utm_source=ig_web_copy_link&igsh=NTc4MTIwNjQ2YQ=='
+    }
 ];
