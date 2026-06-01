23092473d9c77f68fc016ad998b690acb09fb17f
andrescat98 <andrescat98@gmail.com>
2026-06-02 00:11:37 +0200

    rollbkack

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
-    output: 'server',
+    output: 'static',
     vite: {
         plugins: [tailwindcss()]
     },
