b681d147f4b5a5c971d7e9eb301e50be885bfc46
andrescat98 <andrescat98@gmail.com>
2026-06-02 00:05:11 +0200

    server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
+    output: 'server',
     vite: {
         plugins: [tailwindcss()]
     },
